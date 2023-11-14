d0391caa7281ff3edbe14743ee1ba43488abd15f
Michał Lowas-Rzechonek <michal.lowas-rzechonek@silvair.com>
2023-11-14 16:13:38 +0100

    Split bluetooth-mesh into namespace packages

---
+++ bluetooth_mesh/bluez/apps/mixins.py
@@ -0,0 +1,13 @@
+from bluetooth_mesh.bluez.apps import hookimpl
+
+
+class LocalNetworkMixin:
+    async def get_network(self, *args, **kwargs):
+        # TODO: add a way to use a local provisioning database
+
+        raise NotImplementedError("Cannot obtain network database without Silvair commissioning API")
+
+
+@hookimpl(trylast=True)
+def application_mixins():
+    return (LocalNetworkMixin,)
